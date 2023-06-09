f59aba6e4130fcddb072ba9956bf56233bc124ac
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-06-09 18:24:38 -0500

    chore: update eslint and add typescript-eslint parser and plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     node: true
   },
   extends: [
-    "standard-with-typescript",
+    "standard",
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "eslint-config-prettier"
