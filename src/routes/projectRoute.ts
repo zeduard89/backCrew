aaa79135b00b86cdb500fc26ac4b4529652e11d6
Guillermo <zeduard89@gmail.com>
2023-06-09 17:11:46 -0300

    fix: Cambios de capeta util y router

--- src/routes/projectRoute.ts
+++ src/routes/projectRoute.ts
@@ -1,6 +1,6 @@
 import { Router, Request, Response } from 'express'
 import toNewProyects from '../utils/utilsProjects' // Validador del Body
-import proyectos from '../controllers/projects/projectos'
+// import proyectos from '../controllers/projects/postHandler'
 
 const router = Router()
 
@@ -8,9 +8,9 @@ router.post('/', (req: Request, res: Response) => {
   try {
     const projectInfo = toNewProyects(req.body)
 
-    const newProject = proyectController(projectInfo)
+    // const newProject = proyectos(projectInfo)
 
-    res.json(newProject)
+   //  res.json(newProject)
   } catch (error) {
     res.status(400).send(error.message || 'Error Desconocido')
   }
