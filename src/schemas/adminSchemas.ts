2fe904d4ca204be7dd0e6a405625b1052824d57c
ferbragon <nando.gon17@gmail.com>
2023-06-12 11:32:13 -0600

    feat(src/schemas): Add 'zod' schemas for admin

---
+++ src/schemas/adminSchemas.ts
@@ -0,0 +1,9 @@
+import { z } from "zod"
+
+//schemas for Admin
+export const validatorAdmin = z.object({
+    name: z.string().min(3).max(20),
+    lastName: z.string().min(3).max(20),
+    email: z.string().regex(/^(?=.{3,35}@.{4,35}$)(?=.{9,72}$)(?!.\\.\\.)(?!^\\.)(?!.\\.$)[a-zA-Z0-9][a-zA-Z0-9.!#$%&\'+/=?^_`{|}~-][a-zA-Z0-9]@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$/),
+    password: z.string()// (review hash and cripted for this type)z.string.regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$#+-*&])[A-Za-z\d$#+-*&]{8,}$/)
+  })
