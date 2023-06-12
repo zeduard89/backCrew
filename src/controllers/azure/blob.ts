04bf114b7467a4dd5d8f3c6d810fe335880d7280
Guillermo <zeduard89@gmail.com>
2023-06-12 00:14:46 -0300

    feat:azure: se crea la ruta y el controlador para poder obtener las url de los elementos depositados en el contendor seleccionado, tambien se le agrego la propiedad name a las imagenes

--- src/controllers/azure/blob.ts
+++ src/controllers/azure/blob.ts
@@ -1,6 +1,11 @@
 import { Request, Response } from "express"
 // esta clase me permite conectarme con azureCStore
-import { BlobServiceClient } from "@azure/storage-blob"
+import {
+  BlobServiceClient,
+  BlobSASPermissions,
+  generateBlobSASQueryParameters,
+  StorageSharedKeyCredential
+} from "@azure/storage-blob"
 
 // Cargamos las variables de entorno con config y la ejecuto para conectar
 import dotenv from "dotenv"
@@ -15,22 +20,32 @@ const blobService = BlobServiceClient.fromConnectionString(connectionString)
 
 export const uploadBlob = async (req: Request, res: Response) => {
   try {
-    console.log("hola")
     // Verifico si req.file contiene los elementos
     if (!req.file) {
       throw new Error("No se ha proporcionado ningún archivo en la solicitud")
     }
     // Recibimos el nombre del contenedor
-    const { container } = req.body
+    const { container, name } = req.body
     // Recibimos el archivo (su nombre y el buffer)
     const { originalname, buffer } = req.file
-
-    // Buscamos el contenedor
+    if (!container || !name)
+      throw new Error(`Datos del contendor/name incompletos`)
+    // Buscamos si existe el contenedor
     const containerClient = blobService.getContainerClient(container)
+    const containerExist = await containerClient.exists()
+    if (!containerExist) throw new Error(`No existe el container ${container}`)
 
-    // Guardamos el archivo
-    await containerClient.getBlockBlobClient(originalname).uploadData(buffer)
-    res.json({ message: "success" })
+    // Buscamos si existe la imagen
+    const exist = await containerClient
+      .getBlockBlobClient(originalname)
+      .exists()
+    if (exist) throw new Error(`El elemento: ${originalname} ya existe`)
+
+    // Guardamos el archivo con el nombre de la variable name
+    await containerClient.getBlockBlobClient(name).uploadData(buffer)
+    res.json({
+      message: `El elemento: ${name} fue creado Exitosamente`
+    })
   } catch (error) {
     const errorMessage =
       (error as Error).message || "Error desconocido al guardar ImagenAzure"
@@ -38,19 +53,82 @@ export const uploadBlob = async (req: Request, res: Response) => {
   }
 }
 
-// Funcion para ver el Blob
+// Funcion para ver el Blob (Buffer de imagen que renderizar en web con url)
+export const getBlobList = async (req: Request, res: Response) => {
+  try {
+    const { container } = req.params
+
+    // Buscamos si existe el contenedor
+    const containerClient = blobService.getContainerClient(container)
+    const containerExist = await containerClient.exists()
+    if (!containerExist) throw new Error(`No existe el container: ${container}`)
+
+    // Lista los blobs dentro del contenedor
+    const blobList = containerClient.listBlobsFlat()
+
+    // Itera sobre la lista de blobs y genera la URL con firma de acceso compartido (SAS) para cada uno
+    const blobUrls = []
+    for await (const blob of blobList) {
+      const blobClient = containerClient.getBlobClient(blob.name)
+
+      // Genero el permisos para podes acceder a al elemento
+      const sasPermissions = new BlobSASPermissions()
+      sasPermissions.read = true // Set the desired permissions
+      if (!process.env.ACCOUNT_NAME || !process.env.ACCOUNT_KEY)
+        throw new Error("Error en acreditacion Azure")
+      const sasExpiresOn = new Date(new Date().valueOf() + 86400 * 1000) // Expires in 24 hours
+      const sharedKeyCredential = new StorageSharedKeyCredential(
+        process.env.ACCOUNT_NAME,
+        process.env.ACCOUNT_KEY
+      )
+
+      // Genero el token con los detalles necesario
+      const sasToken = generateBlobSASQueryParameters(
+        {
+          containerName: containerClient.containerName,
+          blobName: blobClient.name,
+          permissions: sasPermissions,
+          startsOn: new Date(),
+          expiresOn: sasExpiresOn,
+          contentDisposition: "inline" // Establece el valor "inline" para mostrar el archivo en el navegador
+        },
+        sharedKeyCredential
+      ).toString()
+      // Pusheo en un array los objetos para tenernos ordenados
+      const blobUrlWithSAS = blobClient.url + "?" + sasToken
+      blobUrls.push({
+        name: blobClient.name,
+        url: blobUrlWithSAS
+      })
+      // Termino la iteracion de los elementos
+    }
+    res.status(200).json(blobUrls)
+  } catch (error) {
+    const errorMessage =
+      (error as Error).message || "Error desconocido al buscar ImagenesAzure"
+    res.status(500).send(errorMessage)
+  }
+}
+
+// Funcion para ver el Blob (Buffer de imagen que renderizar en web con url)
 export const getBlob = async (req: Request, res: Response) => {
   try {
     const { container, filename } = req.params
 
+    // Buscamos si existe el contenedor
     const containerClient = blobService.getContainerClient(container)
+    const containerExist = await containerClient.exists()
+    if (!containerExist) throw new Error(`No existe el container: ${container}`)
+
+    // Buscamos si existe la imagen
+    const exist = await containerClient.getBlockBlobClient(filename).exists()
+    if (!exist) throw new Error(`El elemento: ${filename} no existe`)
 
     // obtener el Archivo
     res.header("Content-Type", "image/jpg")
     const response = await containerClient
       .getBlockBlobClient(filename)
       .downloadToBuffer()
-
     res.send(response)
   } catch (error) {
     const errorMessage =
@@ -64,7 +142,14 @@ export const downloadBlob = async (req: Request, res: Response) => {
   try {
     const { container, filename } = req.params
 
+    // Buscamos si existe el contenedor
     const containerClient = blobService.getContainerClient(container)
+    const containerExist = await containerClient.exists()
+    if (!containerExist) throw new Error(`No existe el container: ${container}`)
+
+    // Buscamos si existe la imagen
+    const exist = await containerClient.getBlockBlobClient(filename).exists()
+    if (!exist) throw new Error(`El elemento: ${filename} no existe`)
 
     // Descargamos el Archivo
     const response = await containerClient
@@ -84,14 +169,20 @@ export const deleteBlob = async (req: Request, res: Response) => {
   try {
     const { container, filename } = req.body
 
+    // comprobar si existe el container
     const containerClient = blobService.getContainerClient(container)
+    const containerExist = await containerClient.exists()
+    if (!containerExist) throw new Error(`No existe el container: ${container}`)
 
+    // Comprobar si existe la imagen
+    const exist = await containerClient.getBlockBlobClient(filename).exists()
+    if (!exist) {
+      throw new Error(`No se encuentra el archivo ${filename} en el deposito`) // El error es un String
+    }
     // Descargamos el Archivo
-    const response = await containerClient
-      .getBlockBlobClient(filename)
-      .deleteIfExists()
+    await containerClient.getBlockBlobClient(filename).deleteIfExists()
 
-    res.send(response)
+    res.send({ message: `Se borro con exito el archivo ${filename}` })
   } catch (error) {
     const errorMessage =
       (error as Error).message || "Error desconocido al eliminar ImagenAzure"
