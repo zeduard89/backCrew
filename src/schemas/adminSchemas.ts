eb235d00b7ab1f3b34b5705ef21b8e040710ecfb
ferbragon <nando.gon17@gmail.com>
2023-06-12 14:06:40 -0600

    feat(src/schemas): Add admin to schema like a string

--- src/schemas/adminSchemas.ts
+++ src/schemas/adminSchemas.ts
@@ -2,6 +2,8 @@ import { z } from "zod"
 
 //schemas for Admin
 export const validatorAdmin = z.object({
+    id: z.string(),
+    admin: z.string(),
     name: z.string().min(3).max(20),
     lastName: z.string().min(3).max(20),
     email: z.string().regex(/^(?=.{3,35}@.{4,35}$)(?=.{9,72}$)(?!.\\.\\.)(?!^\\.)(?!.\\.$)[a-zA-Z0-9][a-zA-Z0-9.!#$%&\'+/=?^_`{|}~-][a-zA-Z0-9]@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$/),
