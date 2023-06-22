53bccd1da01b552a3e6f50a33eb9f324df4e5e84
ferbragon <nando.gon17@gmail.com>
2023-06-22 12:29:12 -0600

    feat(src/): Add config to deploy

--- src/config/db.ts
+++ src/config/db.ts
@@ -8,10 +8,10 @@ import UserFavoritesModel from "../models/UserFavoritesModel"
 
 dotenv.config()
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME} = process.env
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER ?? ""}:${DB_PASSWORD ?? ""}@${DB_HOST ?? ""}:${
+  `postgresql://${DB_USER ?? ""}:${DB_PASSWORD ?? ""}@${DB_HOST ?? ""}:${
     DB_PORT ?? ""
   }/${DB_NAME ?? ""}`,
   {
