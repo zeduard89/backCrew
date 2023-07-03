69a14259cab1afbc2da5813063e6bd7790738f14
ferbragon <nando.gon17@gmail.com>
2023-07-03 15:03:06 -0600

    fix(src/constrollers/project): Fix route for the logical project delete

--- src/controllers/projects/deleteProjectByName.ts
+++ src/controllers/projects/deleteProjectByName.ts
@@ -5,48 +5,17 @@ const deleteProyectByNameController = async (
   validatedProject: deleteIProyect
 ): Promise<string> => {
   try {
-    const projectDB = await ProjectModel.findOne({
-      where: { id: validatedProject.id }
-    })
+    const projectDB = await ProjectModel.findOne({ where: { id: validatedProject.id } })
     if (!projectDB) throw new Error("Project not found")
     //* Si la condicion es FALSE la retorno a TRUE
-    if (!validatedProject.displayProject) {
-      const existingProject = await ProjectModel.update(
-        {
-          // Aquí se proporcionan los valores a actualizar
-          displayProject: false
-        },
-        {
-          // Aquí se especifica la condición de búsqueda
-          where: {
-            id: validatedProject.id
-          }
-        }
-      )
-      if (!existingProject) {
-        throw new Error("Project doesn't exist")
-      }
+    projectDB.displayProject = !projectDB.displayProject;
+    await projectDB.save();
 
-      return "Successful change displayProject"
+    if (projectDB.displayProject === false) {
+      return "Project Was Deleted Successfully"
+    } else {
+      return "Project Was Restored Successfully" 
     }
-    //* Si la condicion es TRUE la retorno a FALSE
-    const existingProject = await ProjectModel.update(
-      {
-        // Aquí se proporcionan los valores a actualizar
-        displayProject: true
-      },
-      {
-        // Aquí se especifica la condición de búsqueda
-        where: {
-          id: validatedProject.id
-        }
-      }
-    )
-    if (!existingProject) {
-      throw new Error("Project does not exist")
-    }
-
-    return "Successful change displayProject"
   } catch (error) {
     const errorMessage =
       (error as Error).message || "Unknown error in deleting Project"
