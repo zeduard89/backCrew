7e534427585934b405b176fb320e95478f225973
zeduard89 <zeduard89@gmail.com>
2023-07-04 22:22:29 -0300

    fix: mod server

--- src/config/server.ts
+++ src/config/server.ts
@@ -2,26 +2,41 @@ import express, { ErrorRequestHandler } from "express"
 import cookieParser from "cookie-parser"
 import bodyParser from "body-parser"
 import morgan from "morgan"
+import cors from "cors"
 
 // Importacion de Rutas Dinamicas
 import { router } from "../routes/index"
 
 const server = express()
 
+server.use(
+  cors({
+    origin: "*", // Permitir acceso desde cualquier origen
+    credentials: true
+  })
+)
+
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }))
 server.use(bodyParser.json({ limit: "50mb" }))
 server.use(cookieParser())
 server.use(morgan("dev"))
-server.use((_, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*") // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true")
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  )
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-  next()
-})
+
+// ----------------------------
+
+// server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }))
+// server.use(bodyParser.json({ limit: "50mb" }))
+// server.use(cookieParser())
+// server.use(morgan("dev"))
+// server.use((_, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*") // update to match the domain you will make the request from
+//   res.header("Access-Control-Allow-Credentials", "true")
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   )
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+//   next()
+// })
 
 // Rutas
 server.use("/", router)
