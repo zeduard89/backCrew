b884157e7002174f083a5d9dcab16f00434adfe5
zeduard89 <zeduard89@gmail.com>
2023-07-04 21:56:50 -0300

    fix: server.js access control

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,27 +1,30 @@
-import express, { ErrorRequestHandler } from 'express'
-import cookieParser from 'cookie-parser'
-import bodyParser from 'body-parser'
-import morgan from 'morgan'
+import express, { ErrorRequestHandler } from "express"
+import cookieParser from "cookie-parser"
+import bodyParser from "body-parser"
+import morgan from "morgan"
 
 // Importacion de Rutas Dinamicas
-import { router } from '../routes/index'
+import { router } from "../routes/index"
 
 const server = express()
 
-server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
-server.use(bodyParser.json({ limit: '50mb' }))
+server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }))
+server.use(bodyParser.json({ limit: "50mb" }))
 server.use(cookieParser())
-server.use(morgan('dev'))
+server.use(morgan("dev"))
 server.use((_, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*') // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+  res.header("Access-Control-Allow-Origin", "https://crew-xi.vercel.app") // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  )
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
   next()
 })
 
 // Rutas
-server.use('/', router)
+server.use("/", router)
 
 // Error catching middleware.
 const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
